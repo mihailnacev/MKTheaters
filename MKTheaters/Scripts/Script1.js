11ed3b6059cc71d28103d543053704b61cdab56f
Martina <martina.tosevska.95@gmail.com>
2016-07-07 13:43:45 +0200

    Final colors in master page

--- MKTheaters/Scripts/Script1.js
+++ MKTheaters/Scripts/Script1.js
@@ -1,8 +1,8 @@
 ﻿$(document).ready(function ()
 {
     $(".menuItem").hover(function () {
-        $(this).css("background-color", "lightGrey");
+        $(this).css({ "background-color": "#BA252A", "color": "#F0CB01" });
     }, function () {
-        $(this).css("background-color", "white");
+        $(this).css({ "background-color": "#FFFFFF", "color": "#BA252A" });
     });
-});
+})
