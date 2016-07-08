57cdb3491171112242164c05ae0cc58fbe16000c
Martina <martina.tosevska.95@gmail.com>
2016-07-08 15:07:38 +0200

    Solved problemin menu

--- MKTheaters/Scripts/Script1.js
+++ MKTheaters/Scripts/Script1.js
@@ -5,17 +5,20 @@
         $("#menu a").addClass("menuItem2");
         $("#logo").addClass("logoPosition1");
         $("#menu").addClass("menuPosition");
+        $("#header").addClass("headerHeight3");
     }
     else if ($(window).width() < 1100)
     {
         $("#menu a").addClass("menuItem3");
         $("#logo").addClass("logoPosition1");
         $("#menu").addClass("menuPosition");
+        $("#header").addClass("headerHeight2");
     }
     else
     {
         $("#menu a").addClass("menuItem1");
         $("#logo").addClass("logoPosition2");
+        $("#header").addClass("headerHeight1");
     }
     $("a").hover(function () {
         $(this).css({ "background-color": "#F0CB01", "border": "3px solid #BA252A" });
@@ -34,6 +37,9 @@
             $("#logo").addClass("logoPosition1");
             $("#logo").removeClass("logoPosition2");
             $("#menu").addClass("menuPosition");
+            $("#header").removeClass("headerHeight1");
+            $("#header").removeClass("headerHeight2");
+            $("#header").addClass("headerHeight3");
         }
         else if (w < 1100)
         {
@@ -43,15 +49,21 @@
             $("#logo").addClass("logoPosition1");
             $("#logo").removeClass("logoPosition2");
             $("#menu").addClass("menuPosition");
+            $("#header").removeClass("headerHeight1");
+            $("#header").addClass("headerHeight2");
+            $("#header").removeClass("headerHeight3");
         }
         else
         {
             $("#menu a").addClass("menuItem1");
             $("#menu a").removeClass("menuItem2");
             $("#menu a").removeClass("menuItem3");
-            $("#logo").addClass("logoPosition2");
             $("#logo").removeClass("logoPosition1");
+            $("#logo").addClass("logoPosition2");
             $("#menu").removeClass("menuPosition");
+            $("#header").addClass("headerHeight1");
+            $("#header").removeClass("headerHeight2");
+            $("#header").removeClass("headerHeight3");
         }
     });
 })
