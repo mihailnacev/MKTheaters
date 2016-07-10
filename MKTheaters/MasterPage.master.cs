49462793fe48d3a4be07683a9be15d9ac743e459
Martina <martina.tosevska.95@gmail.com>
2016-07-11 01:59:00 +0200

    Changes in master page

--- MKTheaters/MasterPage.master.cs
+++ MKTheaters/MasterPage.master.cs
@@ -9,6 +9,34 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["Najaven"] != null)
+        {
+            User u = (User)Session["Najaven"];
+            user.Text = u.Username;
+            loggedUser.Visible = true;
+            noLoggedUser.Visible = false;
+        }
+        else
+        {
+            loggedUser.Visible = false;
+            noLoggedUser.Visible = true;
+        }
+    }
 
+    protected void logIn_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Najava.aspx");
+    }
+
+    protected void register_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Registracija.aspx");
+    }
+
+    protected void logOut_Click(object sender, EventArgs e)
+    {
+        Session["Najaven"] = null;
+        loggedUser.Visible = false;
+        noLoggedUser.Visible = true;
     }
 }
