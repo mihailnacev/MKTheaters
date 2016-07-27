57b28471f93047f999a1f79c122964ab892bd076
Magdalena <magdalenaconevska.10@gmail.com>
2016-07-27 18:58:13 +0200

    Napravena shteta

--- MKTheaters/Administracija.aspx
+++ MKTheaters/Administracija.aspx
@@ -21,13 +21,38 @@
 
 <asp:Label runat="server" ID="lblAR" CssClass="info" Text="Репертоар"></asp:Label>
 
-    <asp:GridView ID="gvAllPlays" runat="server"></asp:GridView>
+    
+
+    <asp:GridView ID="gvAllPlays" runat="server" AllowPaging="True" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None" OnPageIndexChanging="gvAllPlays_PageIndexChanging" PageSize="7" OnRowCancelingEdit="gvAllPlays_RowCancelingEdit" OnRowEditing="gvAllPlays_RowEditing" OnRowUpdating="gvAllPlays_RowUpdating">
+        <AlternatingRowStyle BackColor="White" />
+        <Columns>
+            <asp:BoundField DataField="Ime" HeaderText="Претстава" ReadOnly="True" />
+            <asp:BoundField DataField="Avtor" HeaderText="Автор" />
+            <asp:BoundField DataField="Reziser" HeaderText="Режисер" />
+            <asp:BoundField DataField="Akteri" HeaderText="Актери" />
+            <asp:BoundField DataField="Teatar" HeaderText="Театар" />
+            <asp:BoundField DataField="Grad" HeaderText="Град" />
+            <asp:BoundField HeaderText="Датум" DataField="Datum" />
+            <asp:BoundField DataField="Vremetraenje" HeaderText="Времетраење" />
+            <asp:CommandField />
+            <asp:CommandField CancelText="Откажи" EditText="Уреди" ShowEditButton="True" UpdateText="Измени" />
+        </Columns>
+        <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+        <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+        <SortedAscendingCellStyle BackColor="#FDF5AC" />
+        <SortedAscendingHeaderStyle BackColor="#4D0000" />
+        <SortedDescendingCellStyle BackColor="#FCF6C0" />
+        <SortedDescendingHeaderStyle BackColor="#820000" />
+    </asp:GridView>
 
 </asp:Panel>
 
 <asp:Panel ID="pnlPR" CssClass="pnlPR" runat="server">
 
-<asp:Label runat="server" ID="lblOR" CssClass="info" Text="Остварени резервации"></asp:Label>
+<asp:Label runat="server" ID="lblOR" CssClass="info" Text="Остварени резервации" Visible="False"></asp:Label>
     <br />
 
 <asp:GridView ID="gvSeeReservations" runat="server"></asp:GridView>
--- MKTheaters/Administracija.aspx.cs
+++ MKTheaters/Administracija.aspx.cs
@@ -1,14 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
 public partial class Administracija : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+        if (!IsPostBack) {
+            IspolniMaster();
+        }
+
+    }
+
+    public void IspolniMaster() {
+
+        SqlConnection konekcija = new SqlConnection();
+        konekcija.ConnectionString = ConfigurationManager.ConnectionStrings["Test"].ConnectionString;
+        string sqlString = "SELECT * FROM Repertoar";
+        SqlCommand komanda = new SqlCommand(sqlString, konekcija);
+        SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+        DataSet ds = new DataSet();
+
+        try
+        {
+
+            konekcija.Open();
+            adapter.Fill(ds, "Repertoar");
+            gvAllPlays.DataSource = ds;
+            gvAllPlays.DataBind();
+            ViewState["datasetVS"] = ds;
+
+        }
+        catch (Exception err)
+        {
+
+
+        }
+        finally {
+
+            konekcija.Close();
+        }
+
 
     }
 
@@ -22,5 +60,86 @@ public partial class Administracija : System.Web.UI.Page
     {
         pnlPR.Visible = true;
         pnlAR.Visible = false;
+    }
+
+    protected void gvAllPlays_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvAllPlays.PageIndex = e.NewPageIndex;
+        gvAllPlays.SelectedIndex = -1;
+        DataSet ds = (DataSet)ViewState["datasetVS"];
+        gvAllPlays.DataSource = ds;
+        gvAllPlays.DataBind();
+    }
+
+    protected void gvAllPlays_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        DataSet ds = (DataSet)ViewState["datasetVS"];
+        gvAllPlays.DataSource = ds;
+        gvAllPlays.DataBind();
+    }
+
+    protected void gvAllPlays_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        DataSet ds = (DataSet)ViewState["datatsetVS"];
+        gvAllPlays.EditIndex = -1;
+        gvAllPlays.DataSource = ds;
+        gvAllPlays.DataBind();
+    }
+
+    protected void gvAllPlays_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        SqlConnection konekcija = new SqlConnection();
+        konekcija.ConnectionString= ConfigurationManager.ConnectionStrings["Test"].ConnectionString;
+        string sqlString = "UPDATE Repertoar set" +
+            " Avtor=@Avtor, Reziser=@Reziser, Akteri=@Akteri, Teatar=@Teatar, Grad=@Grad, Datum=@Datum, Vremetraenje=@Vremetraenje WHERE Ime=@Ime";
+        SqlCommand komanda = new SqlCommand(sqlString, konekcija);
+
+   
+
+        TextBox tbAvtor = (TextBox)gvAllPlays.Rows[e.RowIndex].Cells[1].Controls[0];
+        komanda.Parameters.AddWithValue("@Avtor", tbAvtor.Text);
+
+        TextBox tbReziser = (TextBox)gvAllPlays.Rows[e.RowIndex].Cells[2].Controls[0];
+        komanda.Parameters.AddWithValue("@Reziser", tbReziser.Text);
+
+        TextBox tbAkteri = (TextBox)gvAllPlays.Rows[e.RowIndex].Cells[3].Controls[0];
+        komanda.Parameters.AddWithValue("@Akteri", tbAkteri.Text);
+
+        TextBox tbTeatar = (TextBox)gvAllPlays.Rows[e.RowIndex].Cells[4].Controls[0];
+        komanda.Parameters.AddWithValue("@Teatar", tbTeatar.Text);
+
+        TextBox tbGrad = (TextBox)gvAllPlays.Rows[e.RowIndex].Cells[5].Controls[0];
+        komanda.Parameters.AddWithValue("@Grad", tbGrad.Text);
+
+        TextBox tbDatum = (TextBox)gvAllPlays.Rows[e.RowIndex].Cells[6].Controls[0];
+        komanda.Parameters.AddWithValue("@Datum", tbDatum.Text);
+
+        TextBox tbVremetraenje = (TextBox)gvAllPlays.Rows[e.RowIndex].Cells[7].Controls[0];
+        komanda.Parameters.AddWithValue("@Vremetraenje ", tbVremetraenje.Text);
+
+        komanda.Parameters.AddWithValue("@Ime", gvAllPlays.Rows[e.RowIndex].Cells[0].Text);
+
+        int efekt = 0;
+
+        try
+        {
+
+            konekcija.Open();
+            efekt = komanda.ExecuteNonQuery();
+        }
+        catch
+        {
+
+        }
+        finally {
+
+            konekcija.Close();
+            gvAllPlays.EditIndex = -1;
+
+        }
+
+        if (efekt != 0) {
+            IspolniMaster();
+        }
     }
 }
